1492d41ddc05e06185a945310b4322968c58a45f
mukhammadjondev <muhsdev@gmail.com>
2024-10-19 19:38:00 +0500

    make clerable select and optimize

--- src/components/fields/Select.tsx
+++ src/components/fields/Select.tsx
@@ -1,60 +1,78 @@
 import { ReactNode } from "react"
+import { SelectProps } from "@radix-ui/react-select"
 import { useFormContext } from "react-hook-form"
 
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import {
   Select,
   SelectContent,
+  SelectContentProps,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
 
-interface IProps {
-  name: string
-  label?: ReactNode
-  data: { label: string; value: string; disabled?: boolean }[]
-  placeholder?: string
-  required?: boolean
+export interface IOption {
+  label: string
+  value: string
+  disabled?: boolean
 }
 
-export default function SelectField({
+interface IProps {
+  name: string
+  placeholder?: string
+  data: IOption[]
+  label?: ReactNode
+  required?: boolean
+  clearable?: boolean
+  selectProps?: Omit<SelectProps, 'value' | 'onValueChange' | 'defaultValue' | 'name'>
+  contentProps?: SelectContentProps
+}
+
+export const SelectField = ({
+  name,
   data,
   placeholder,
-  name,
   label,
   required,
-}: IProps) {
+  clearable,
+  selectProps = {},
+  contentProps = {},
+}: IProps) => {
   const { control } = useFormContext()
 
   return (
     <FormField
-      control={control}
       name={name}
+      control={control}
       render={({ field }) => (
-        <FormItem>
+        <FormItem className="space-y-3">
           {label && (
             <FormLabel>
               {label} {required && <span className="text-red-500 dark:text-red-900">*</span>}
             </FormLabel>
           )}
           <FormControl>
-            <Select onValueChange={field.onChange} defaultValue={field.value}>
-              <SelectTrigger>
+            <Select
+              value={field.value}
+              onValueChange={(value) =>
+                field.onChange({
+                  target: {
+                    value,
+                  },
+                })
+              }
+              {...selectProps}
+            >
+              <SelectTrigger
+                clearable={clearable && !!field.value}
+                onClear={() => field.onChange({ target: { value: '' } })}
+              >
                 <SelectValue placeholder={placeholder} />
               </SelectTrigger>
-              <SelectContent>
+              <SelectContent {...contentProps}>
                 {data.map((item) => (
-                  <SelectItem
-                    value={item.value}
-                    key={item.value}
-                    disabled={item.disabled}>
+                  <SelectItem key={item.value} value={item.value} disabled={item.disabled}>
                     {item.label}
                   </SelectItem>
                 ))}
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
 
 import * as SelectPrimitive from "@radix-ui/react-select"
-import { Check, ChevronDown, ChevronUp } from "lucide-react"
+import { Check, ChevronDown, ChevronUp, X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
+import { Button } from "./button"
 
 const Select = SelectPrimitive.Root
 
@@ -11,24 +12,46 @@ const SelectGroup = SelectPrimitive.Group
 
 const SelectValue = SelectPrimitive.Value
 
+interface SelectTriggerProps
+  extends React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger> {
+  clearable?: boolean
+  onClear?: () => void
+}
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300",
-      className,
+  SelectTriggerProps
+>(({ className, children, clearable = false, onClear, ...props }, ref) => (
+  <div className="relative">
+    <SelectPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        'data-[placeholder]:text-gray-500 [&>svg]:data-[state=open]:rotate-180 flex h-11 w-full items-center justify-between rounded-[2px] border border-gray-100  bg-background px-[16px] py-[12px] data-[state=open]:rounded-b-none text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:none focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+        className
+      )}
+      {...props}
+    >
+      {children}
+      {!clearable && (
+        <SelectPrimitive.Icon asChild>
+          <ChevronDown className="h-4 w-4 opacity-50" />
+        </SelectPrimitive.Icon>
+      )}
+    </SelectPrimitive.Trigger>
+    {clearable && (
+      <Button
+        type="button"
+        onClick={onClear}
+        className="w-6 h-6 text-gray-500 absolute top-1/2 -translate-y-1/2 right-4"
+        variant="ghost"
+        size="icon"
+      >
+        <X size={18} />
+      </Button>
     )}
-    {...props}>
-    {children}
-    <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
-    </SelectPrimitive.Icon>
-  </SelectPrimitive.Trigger>
+  </div>
 ))
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
+export type SelectContentProps = React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
 
 const SelectScrollUpButton = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.ScrollUpButton>,
